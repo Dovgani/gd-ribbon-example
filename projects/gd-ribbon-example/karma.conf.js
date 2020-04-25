18d0e9d501a64ba2c4859464781b6b404e29b177
Ghennadii Dovgani <dovgani@comcast.net>
2020-04-25 19:59:14 -0400

    Project name changed to gd-ribbon-example

--- projects/gd-ribbon-example/karma.conf.js
+++ projects/gd-ribbon-example/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage/ribbon-example'),
+      dir: require('path').join(__dirname, '../../coverage/gd-ribbon-example'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- projects/gd-ribbon-example/src/app/app.component.spec.ts
+++ projects/gd-ribbon-example/src/app/app.component.spec.ts
@@ -16,7 +16,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ribbon-example'`, () => {
+  it(`should have as title 'gd-ribbon-example'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
   });
@@ -25,6 +25,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ribbon-example app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('gd-ribbon-example app is running!');
   });
 });
