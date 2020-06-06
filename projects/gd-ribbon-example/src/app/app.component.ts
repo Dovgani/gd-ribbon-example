7904c03ed538e79457f92304a7128f0f3e136f12
Ghennadii Dovgani <dovgani@comcast.net>
2020-06-06 16:30:50 -0400

    GDCanvas is the last large control

--- projects/gd-ribbon-example/src/app/app.component.ts
+++ projects/gd-ribbon-example/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component         } from '@angular/core';
-import { ViewChild         } from '@angular/core';
-import { AfterViewInit     } from '@angular/core';
-import { ChangeDetectorRef } from '@angular/core';
+import { Component                } from '@angular/core';
+import { ViewChild                } from '@angular/core';
+import { AfterViewInit            } from '@angular/core';
+import { ChangeDetectorRef        } from '@angular/core';
 
-import { GDWindowComponent } from 'gd-window';
-import { GDRibbonComponent } from 'gd-ribbon';
+import { SplitterPanels           } from 'gd-common';
+import { ISplitter                } from 'gd-common';
+import { GDCommonService          } from 'gd-common';
+
+import { GDWindowComponent        } from 'gd-window';
+import { GDRibbonComponent        } from 'gd-ribbon';
+import { GDCanvasWrapperComponent } from 'gd-canvas-wrapper';
+import { GDCanvasWrapperService   } from 'gd-canvas-wrapper';
+import { GDContextmenuComponent   } from 'gd-contextmenu';
+import { GDPGComponent            } from 'gd-pg';
 
 @Component({
     selector: 'app-root',
@@ -13,8 +21,43 @@ import { GDRibbonComponent } from 'gd-ribbon';
 })
 export class AppComponent implements AfterViewInit
 {
-    @ViewChild('myWindow')  window : GDWindowComponent;
-    @ViewChild('ribbon')    ribbon : GDRibbonComponent;
+    @ViewChild('myWindow')                window                 : GDWindowComponent;
+    @ViewChild('help')                    help                   : GDWindowComponent;
+    @ViewChild('ribbon')                  ribbon                 : GDRibbonComponent;
+    @ViewChild('canvas')                  canvas                 : GDCanvasWrapperComponent;
+    @ViewChild('canvasShapeMenu')         canvasShapeMenu        : GDContextmenuComponent;
+    @ViewChild('canvasAreaMenu')          canvasAreaMenu         : GDContextmenuComponent;
+    @ViewChild('canvasTextMenu')          canvasTextMenu         : GDContextmenuComponent;
+    @ViewChild('canvasBlockMenu')         canvasBlockMenu        : GDContextmenuComponent;
+    @ViewChild('canvasPolygonMenu')       canvasPolygonMenu      : GDContextmenuComponent;
+    @ViewChild('canvasPolygonPointMenu')  canvasPolygonPointMenu : GDContextmenuComponent;
+    @ViewChild('canvasCalloutMenu')       canvasCalloutMenu      : GDContextmenuComponent;
+    @ViewChild('canvasRefMenu')           canvasRefMenu          : GDContextmenuComponent;
+    @ViewChild('canvasPolylineMenu')      canvasPolylineMenu     : GDContextmenuComponent;
+    @ViewChild('canvasPolylineSegMenu')   canvasPolylineSegMenu  : GDContextmenuComponent;
+    @ViewChild('canvasPolylinePointMenu') canvasPolylinePointMenu: GDContextmenuComponent;
+    @ViewChild('canvasGondolaMenu')       canvasGondolaMenu      : GDContextmenuComponent;
+    @ViewChild('canvasSegMenu')           canvasSegMenu          : GDContextmenuComponent;
+    @ViewChild('canvasDimMenu')           canvasDimMenu          : GDContextmenuComponent;
+    @ViewChild('canvasArcMenu')           canvasArcMenu          : GDContextmenuComponent;
+    @ViewChild('canvasDrawingMenu')       canvasDrawingMenu      : GDContextmenuComponent;
+    @ViewChild('canvasPGWindow')          canvasPGWindow         : GDWindowComponent;
+    @ViewChild('canvasPGShape')           canvasPGShape          : GDPGComponent;
+    @ViewChild('canvasPGSeg')             canvasPGSeg            : GDPGComponent;
+    @ViewChild('canvasPGDim')             canvasPGDim            : GDPGComponent;
+    @ViewChild('canvasPGArc')             canvasPGArc            : GDPGComponent;
+    @ViewChild('canvasPGBlock')           canvasPGBlock          : GDPGComponent;
+    @ViewChild('canvasPGRef')             canvasPGRef            : GDPGComponent;
+    @ViewChild('canvasPGText')            canvasPGText           : GDPGComponent;
+    @ViewChild('canvasPGPolyline')        canvasPGPolyline       : GDPGComponent;
+    @ViewChild('canvasPGCallout')         canvasPGCallout        : GDPGComponent;
+    @ViewChild('canvasPGArea')            canvasPGArea           : GDPGComponent;
+    @ViewChild('canvasPGGondola')         canvasPGGondola        : GDPGComponent;
+    @ViewChild('canvasPGDrawing')         canvasPGDrawing        : GDPGComponent;
+
+    @ViewChild('canvasWarningRecursion')      canvasWarningRecursion      : GDWindowComponent;
+    @ViewChild('canvasWarningNotImplemented') canvasWarningNotImplemented : GDWindowComponent;
+    @ViewChild('canvasDeleteConfirm')         canvasDeleteConfirm         : GDWindowComponent;    
 
     public  SearchCustomID = '';
     public  ActiveUser     = 'Jhon Smith';
@@ -24,7 +67,9 @@ export class AppComponent implements AfterViewInit
     public  Project        = 'myProject';
     public  CustomID       = '123';
 
-    public  constructor( private cdRef : ChangeDetectorRef )
+    public  splitterRibbon : ISplitter;
+
+    public  constructor( private cdRef : ChangeDetectorRef,private service : GDCommonService, private service1 : GDCanvasWrapperService )
     {
     }
 
@@ -33,23 +78,241 @@ export class AppComponent implements AfterViewInit
         // disable browser contextmenu
         document.addEventListener( 'contextmenu', (event : any) => event.preventDefault() );   
 
+        window.addEventListener( 'beforeunload', (e) => { 
+            //e.preventDefault(); 
+            //e.returnValue = ''; 
+            this.service.onBeforeBrowserClose();
+        });
+
+        this.getRoot();
+
+        this.splitterRibbon = 
+        {
+            buttonSize : 0, 
+            panels :
+            [
+                {
+                    layout             : SplitterPanels.top, 
+                    isOpened           : true,
+                    isButtonVisible    : false,
+                    isSeperatorVisible : true,
+                    isSeperatorLocked  : true,
+                    seperatorThickness : 1,
+                    size               : 116 
+                }
+            ]    
+        };
+
+        this.help.OK              = 'Yes';
+        this.help.Cancel          = 'No';
+        this.help.IsIconVisible   = true;
+        this.help.Icon            = './assets/Images/favicon.ico';
+        this.help.Title           = 'Canvas features ...';
+        this.help.IsFooterVisible = false;
+        this.help.Y =   40;
+        this.help.X = 1260;
+        this.help.W =  440;
+        this.help.H =  380;
+        this.help.Open();
+
         this.window.AddClass('gd-ribbon');    
         this.window.IsIconVisible = true;
         this.window.Icon          = './assets/Images/favicon.ico';
         this.window.Title         = 'GD Ribbon';
-        this.window.Y =  30;
+        this.window.Y =  40;
         this.window.X =  40;
-        this.window.W = 964;
-        this.window.H = 300;
+        this.window.W = 968;
+        this.window.H = 740;
         this.window.Open();
 
-        this.ribbon.ParentID = 'gd_window_' + this.window.ID;
+        this.ribbon.ParentID            = 'gd_window_' + this.window.ID;
+        this.canvas.ParentID            = 'gd_window_' + this.window.ID;
+        this.canvas.menuShape           = this.canvasShapeMenu;
+        this.canvas.menuArea            = this.canvasAreaMenu;
+        this.canvas.menuGondola         = this.canvasGondolaMenu;
+        this.canvas.menuPolyline        = this.canvasPolylineMenu;
+        this.canvas.menuPolylinePoint   = this.canvasPolylinePointMenu;
+        this.canvas.menuPolylineSeg     = this.canvasPolylineSegMenu;
+        this.canvas.menuCallout         = this.canvasCalloutMenu;
+        this.canvas.menuText            = this.canvasTextMenu;
+        this.canvas.menuBlock           = this.canvasBlockMenu;
+        this.canvas.menuPolygon         = this.canvasPolygonMenu;
+        this.canvas.menuRef             = this.canvasRefMenu;
+        this.canvas.menuArc             = this.canvasArcMenu;
+        this.canvas.menuSeg             = this.canvasSegMenu;
+        this.canvas.menuDim             = this.canvasDimMenu;
+        this.canvas.menuDrawing         = this.canvasDrawingMenu;
+        this.canvas.pgWindow            = this.canvasPGWindow;
+
+        this.canvas.warningRecursion      = this.canvasWarningRecursion;
+        this.canvas.warningNotImplemented = this.canvasWarningNotImplemented;
+        this.canvas.deleteConfirm         = this.canvasDeleteConfirm;    
     }    
 
     public  ngAfterViewChecked() 
     {
         this.cdRef.detectChanges();
     }
+
+    private getRoot()
+    {
+        this.service1.getRoot().subscribe(
+            shapes => this.OnLoadComplated_GetRoot( shapes ),
+            error  => this.OnRequestError         ( error  )
+        );
+    }
+
+    private OnLoadComplated_GetRoot( model : Root )
+    {
+        this.canvas.containerChildWidth  = model.areaChildWidth;
+        this.canvas.containerChildHeight = model.areaChildHeight;
+
+//        this.setCookie( 'rootInfo', { 'root': root }, null );        
+    }
+
+    private OnRequestError( error : any )
+    {
+        alert( error );
+    }
+
+    //#region CONTEXT MENU
+    public  onShapeMenuItemClick( item : string )
+    {
+        switch( item )
+        {
+            case 'Edit'   :  this.canvas.OpenShapePG();          break;
+            case 'Copy'   :  this.canvas.onNotImplemented(null); break;
+            case 'Delete' :  this.canvas.onDeleteConfirm(null);  break;
+        }
+    }     
+
+    public  onPolygonMenuItemClick( item : string )
+    {
+        switch( item )
+        {
+            case 'Edit'   :  this.canvas.OpenPolygonPG();        break;
+            case 'Copy'   :  this.canvas.onNotImplemented(null); break;
+            case 'Delete' :  this.canvas.onDeleteConfirm(null);  break;
+        }
+    }   
+
+    public  onAreaMenuItemClick( item : string )
+    {
+        switch( item )
+        {
+            case 'Edit'   :  this.canvas.OpenAreaPG();           break;
+            case 'Copy'   :  this.canvas.onNotImplemented(null); break;
+            case 'Delete' :  this.canvas.onDeleteConfirm(null);  break;
+        }
+    }     
+
+    public  onSegMenuItemClick( item : string )
+    {
+        switch( item )
+        {
+            case 'Edit'   :  this.canvas.OpenSegPG();            break;
+            case 'Copy'   :  this.canvas.onNotImplemented(null); break;
+            case 'Delete' :  this.canvas.onDeleteConfirm(null);  break;
+        }
+    }     
+
+    public  onDimMenuItemClick( item : string )
+    {
+        switch( item )
+        {
+            case 'Edit'   :  this.canvas.OpenDimPG();            break;
+            case 'Copy'   :  this.canvas.onNotImplemented(null); break;
+            case 'Delete' :  this.canvas.onDeleteConfirm(null);  break;
+        }
+    }     
+
+    public  onArcMenuItemClick( item : string )
+    {
+        switch( item )
+        {
+            case 'Edit'   :  this.canvas.OpenArcPG();            break;
+            case 'Copy'   :  this.canvas.onNotImplemented(null); break;
+            case 'Delete' :  this.canvas.onDeleteConfirm(null);  break;
+        }
+    }     
+
+    public  onTextMenuItemClick( item : string )
+    {
+        switch( item )
+        {
+            case 'Edit'   :  this.canvas.OpenTextPG();           break;
+            case 'Copy'   :  this.canvas.onNotImplemented(null); break;
+            case 'Delete' :  this.canvas.onDeleteConfirm(null);  break;
+        }
+    }     
+
+    public  onPolylineMenuItemClick( item : string )
+    {
+        switch( item )
+        {
+            case 'Edit'      :  this.canvas.OpenPolylinePG();                   break;
+            case 'Copy'      :  this.canvas.onNotImplemented(null);             break;
+            case 'Delete'    :  this.canvas.onDeleteConfirm(null);              break;
+            case 'Line'      :  this.canvas.onSetPolylineSegmentShape('line');  break;
+            case 'Arc'       :  this.canvas.onSetPolylineSegmentShape('arc');   break;
+            case 'Add Point' :  this.canvas.addPolylinePoint();                 break;
+        }
+    }     
+
+    public  onPolylinePointMenuItemClick( item : string )
+    {
+        switch( item )
+        {
+            case 'Delete' :  this.canvas.deletePolylinePoint();  break;
+        }
+    }     
+
+    public  onRefMenuItemClick( item : string )
+    {
+        switch( item )
+        {
+            case 'Edit'   :  this.canvas.OpenRefPG();            break;
+            case 'Copy'   :  this.canvas.onNotImplemented(null); break;
+            case 'Delete' :  this.canvas.onDeleteConfirm(null);  break;
+        }
+    }     
+
+    public  onCalloutMenuItemClick( item : string )
+    {
+        switch( item )
+        {
+            case 'Edit'   :  this.canvas.OpenCalloutPG();        break;
+            case 'Copy'   :  this.canvas.onNotImplemented(null); break;
+            case 'Delete' :  this.canvas.onDeleteConfirm(null);  break;
+        }
+    }     
+
+    public  onPolygonPointMenuItemClick( item : string )
+    {
+        switch( item )
+        {
+            case 'Add'    :  this.canvas.onNotImplemented(null); break;
+            case 'Delete' :  this.canvas.onDeleteConfirm(null);  break;
+        }
+    }     
+
+    public  onBlockMenuItemClick( item : string )
+    {
+        switch( item )
+        {
+            case 'Delete' :  this.canvas.onDeleteConfirm(null);  break;
+        }
+    }     
+
+    public  onDrawingMenuItemClick( item : string )
+    {
+        switch( item )
+        {
+            case 'Edit'  :  this.canvas.OpenDrawingPG();         break;
+            case 'Paste' :  this.canvas.onNotImplemented(null);  break;
+        }
+    }     
+    //#endregion
 
     public  onRibbonClick( arg : string )
     {
@@ -79,4 +342,32 @@ export class AppComponent implements AfterViewInit
             case 'zoom-default-btn'             : alert(arg);   break;
         }
     }
+}
+
+class Root
+{
+    id                  : number;
+    folder              : string;
+    folderID            : number;
+    store               : string;
+    storeID             : number;
+    solution            : string;
+    solutionID          : number;
+    project             : string;
+    projectID           : number;
+    prjFixture          : string;
+    prjFixtureID        : number;
+    property            : string;
+    propertyID          : number;
+    fixture             : string;
+    fixtureID           : number;
+    activeStoreID	    : string;
+    activeStoreName	    : string;
+    activeStorePath	    : string;
+    activeSolutionID    : string;
+    activeSolutionName  : string;
+    activeProjectID	    : string;
+    activeProjectName	: string;
+    areaChildWidth   	: number;
+    areaChildHeight		: number;
 }
--- projects/gd-ribbon-example/src/app/app.module.ts
+++ projects/gd-ribbon-example/src/app/app.module.ts
@@ -1,27 +1,48 @@
-import { FormsModule    } from '@angular/forms';
-import { BrowserModule  } from '@angular/platform-browser';
-import { NgModule       } from '@angular/core';
-import { CommonModule   } from '@angular/common'; 
+import { FormsModule               } from '@angular/forms';
+import { BrowserModule             } from '@angular/platform-browser';
+import { NgModule                  } from '@angular/core';
+import { HttpClientModule          } from '@angular/common/http';
 
-import { GDWindowModule } from 'gd-window';
-import { GDRibbonModule } from 'gd-ribbon';
+import { CommonModule              } from '@angular/common'; 
 
-import { AppComponent   } from './app.component';
+import { GDCanvasModule            } from 'gd-canvas';
+import { GDCanvasWrapperModule     } from 'gd-canvas-wrapper';
+import { GDCanvasCustomTypesModule } from 'gd-canvas-custom-types';
+import { GDPGModule                } from 'gd-pg';
+import { GDAccordionModule         } from 'gd-accordion';
+import { GDNumberEditorModule      } from 'gd-numbereditor';
+import { GDWindowModule            } from 'gd-window';
+import { GDRibbonModule            } from 'gd-ribbon';
+import { GDTooltipModule           } from 'gd-tooltip';
+import { GDSplitterModule          } from 'gd-splitter';
+import { GDContextmenuModule       } from 'gd-contextmenu';
+
+import { AppComponent              } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     FormsModule,
     CommonModule,
 
+    GDCanvasModule,
+    GDCanvasWrapperModule,
     GDRibbonModule,
-    GDWindowModule
+    GDPGModule,
+    GDAccordionModule,
+    GDNumberEditorModule,
+    GDWindowModule,
+    GDSplitterModule,
+    GDCanvasCustomTypesModule,
+    GDContextmenuModule,
+    GDTooltipModule
 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
